8aabe163dcbbb5c2704dc695176af50d99066689
ViniciusTanaca <vinicius_tanaca@outlook.com>
2025-04-25 20:06:43 -0300

    Configurar evento DOMContentLoaded e referências aos elementos HTML

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('tarefa-form');
+    const input = document.getElementById('tarefa-input');
+    const lista = document.getElementById('tarefa-lista');
+    const botaoLimpar = document.getElementById('limpar-lista');
+});
